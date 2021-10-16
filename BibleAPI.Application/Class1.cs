91df0e5627a3751c748932b6bb8b1b34c4c25e13
Wyatt Baggett <wyattbaggett@yahoo.com>
2021-10-15 22:28:54 -0400

    Setup foundation for the API

---
+++ BibleAPI.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BibleAPI.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ BibleAPI.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BibleAPI.Core
+{
+    public class Class1
+    {
+    }
+}
